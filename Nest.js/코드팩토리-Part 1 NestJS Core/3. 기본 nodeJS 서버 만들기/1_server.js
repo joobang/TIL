d10a6517bc847c65cb6198129d36323a741b9e8d
dojoohwan <ehwnghks@gmail.com>
2024-07-27 19:24:35 +0900

    path 별로 다른 응답 변환하기

--- Nest.js/코드팩토리-Part 1 NestJS Core/3. 기본 nodeJS 서버 만들기/1_server.js
+++ Nest.js/코드팩토리-Part 1 NestJS Core/3. 기본 nodeJS 서버 만들기/1_server.js
@@ -1,11 +1,25 @@
 const http = require("http");
+const url = require("url");
 
 const host = "localhost";
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/html" });
-  res.end("<h1>Hello World</h1>");
+  const path = url.parse(req.url).pathname;
+
+  if (path === "/") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end("<h1>Home page!</h1>");
+  } else if (path == "/post") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end("<h1>Post page!</h1>");
+  } else if (path == "/user") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end("<h1>User page!</h1>");
+  } else {
+    res.writeHead(404, { "Content-Type": "text/html" });
+    res.end("<h1>Not Found Page</h1>");
+  }
 });
 
 server.listen(port, host, () => {
