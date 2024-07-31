5dc40b84d7c3ed4e7f1cffd96603592b440d5f07
dojoohwan <ehwnghks@gmail.com>
2024-07-31 20:14:11 +0900

    Get Posts 구현하기

--- Nest.js/CodeFactory_NestJS_Core/CF_SNS/src/posts/posts.controller.ts
+++ Nest.js/CodeFactory_NestJS_Core/CF_SNS/src/posts/posts.controller.ts
@@ -9,25 +9,61 @@ import { PostsService } from './posts.service';
  * commentCount: number;
  */
 
-interface Post {
+interface PostModel {
+  id: number;
   author: string;
   title: string;
   content: string;
   likeCount: number;
   commentCount: number;
 }
+
+const posts: PostModel[] = [
+  {
+    id: 1,
+    author: 'newjeans_official',
+    title: '뉴진스 민지',
+    content: '메이크업 고치고 있는 민지',
+    likeCount: 10000000,
+    commentCount: 999999,
+  },
+  {
+    id: 2,
+    author: 'newjeans_official',
+    title: '뉴진스 해린',
+    content: '노래 하고 있는 해린',
+    likeCount: 10000000,
+    commentCount: 999999,
+  },
+  {
+    id: 3,
+    author: 'blackpink_official',
+    title: '블랙핑크 로제',
+    content: '종합운동장에서 공연하는 로제',
+    likeCount: 10000000,
+    commentCount: 999999,
+  },
+];
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
 
+  // 1) GET /posts
+  // 모든 포스트를 가져온다.
   @Get()
-  getPost(): Post {
-    return {
-      author: 'newjeans_official',
-      title: '뉴진스 민지',
-      content: '메이크업 고치고 있는 민지',
-      likeCount: 10000000,
-      commentCount: 999999,
-    };
+  getPosts() {
+    return posts;
   }
+
+  // 2) GET /posts/:id
+  // id에 해당하는 포스트를 가져온다.
+
+  // 3) POST /posts
+  //   POST를 생성한다.
+
+  // 4) PUT /posts/:id
+  //   id에 해당되는 POST를 변경한다.
+
+  // 5) DELETE /posts/:id
+  //   id에 해당되는 POST를 삭제한다.
 }
